4ef81356f0439a48d66e50980fb5246f8a26e116
Ibrahim-Y-Adam <ibroos@hotmail.com>
2023-09-06 01:59:04 +0100

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,7 +77,7 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else if (operator === "*") {
+    } else if (operator === "x") {
         return [operand1 * operand2, "multiply"]
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"]
@@ -115,8 +115,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
     
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand2 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 }
 
@@ -124,5 +124,5 @@ function displayMultiplyQuestion(operand1, operand2) {
     
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "*";
+    document.getElementById("operator").textContent = "x";
 }
